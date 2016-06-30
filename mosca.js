9835bae6a960f10bc27c6501de4d0d41cf323555
aborovsky <kycok.6o6pa@gmail.com>
2016-06-30 07:32:22 +0300

    Few persistent providers suppport implemented

--- mosca.js
+++ mosca.js
@@ -18,98 +18,106 @@ var RED = require(process.env.NODE_RED_HOME + "/red/red");
 var mosca = require('mosca');
 
 function MoscaInNode(n) {
-    RED.nodes.createNode(this, n);
-    this.dburl = n.dburl ? n.dburl.toString() : '';
-    this.mqtt_port = parseInt(n.mqtt_port);
-    this.mqtt_ws_port = parseInt(n.mqtt_ws_port);
-    var moscaSettings = {
-        interfaces: []
-    };
+  RED.nodes.createNode(this, n);
+  this.dbtype = n.dbtype ? n.dbtype.toString() : '';
+  this.dburl = n.dburl ? n.dburl.toString() : '';
+  this.mqtt_port = parseInt(n.mqtt_port);
+  this.mqtt_ws_port = parseInt(n.mqtt_ws_port);
+  var moscaSettings = {
+    interfaces: []
+  };
 
-    if (this.mqtt_port)
-        moscaSettings.interfaces.push({type: "mqtt", port: this.mqtt_port});
-    if (this.mqtt_ws_port)
-        moscaSettings.interfaces.push({type: "http", port: this.mqtt_ws_port});
-    //TODO: read https://github.com/mcollina/mosca/blob/master/lib/server.js and add support of mqtts and wss
+  if (this.mqtt_port)
+    moscaSettings.interfaces.push({type: "mqtt", port: this.mqtt_port});
+  if (this.mqtt_ws_port)
+    moscaSettings.interfaces.push({type: "http", port: this.mqtt_ws_port});
+  //TODO: read https://github.com/mcollina/mosca/blob/master/lib/server.js and add support of mqtts and wss
 
-    var node = this;
-    node.log("Binding mosca mqtt server on port: " + this.mqtt_port);
-    var server = new mosca.Server(moscaSettings, function (err) {
-        if (err) {
-            err.msg = 'Error binding mosca mqtt server, cause: ' + err.toString();
-            node.error(err);
-        }
-    });
-
-    server.on('clientConnected', function (client) {
-        var msg = {
-            topic: "clientConnected",
-            payload: client
-        };
-        node.send(msg);
-    });
-
-    server.on('clientDisconnected', function (client) {
-        var msg = {
-            topic: "clientDisconnected",
-            payload: client
-        };
-        node.send(msg);
-    });
-
-    server.on('published', function (packet, client) {
-        var msg = {
-            topic: "published",
-            payload: {
-                packet: packet,
-                client: client
-            }
-        };
-        node.send(msg);
-    });
-
-    server.on('subscribed', function (topic, client) {
-        var msg = {
-            topic: "subscribed",
-            payload: {
-                topic: topic,
-                client: client
-            }
-        };
-        node.send(msg);
-    });
-
-    server.on('unsubscribed', function (topic, client) {
-        var msg = {
-            topic: "unsubscribed",
-            payload: {
-                topic: topic,
-                client: client
-            }
-        };
-        node.send(msg);
-    });
-
-    this.on('close', function () {
-        node.log("Unbinding mosca mqtt server from port: " + this.port);
-        server.close();
-        server.removeAllListeners('clientConnected');
-        server.removeAllListeners('clientDisconnected');
-        server.removeAllListeners('published');
-        server.removeAllListeners('subscribed');
-        server.removeAllListeners('unsubscribed');
-        
-    });
-    if (this.dburl) {
-        var onPersistenceReady = function () {
-            node.log('Persistence Ready');
-            persistence.wire(server);
-        }
-
-        var persistenceOpts = {
-            url: this.dburl
-        }
-        var persistence = mosca.persistence.Mongo(persistenceOpts, onPersistenceReady);
+  var node = this;
+  node.log("Binding mosca mqtt server on port: " + this.mqtt_port);
+  var server = new mosca.Server(moscaSettings, function(err) {
+    if (err) {
+      err.msg = 'Error binding mosca mqtt server, cause: ' + err.toString();
+      node.error(err);
     }
+  });
+
+  server.on('clientConnected', function(client) {
+    var msg = {
+      topic: "clientConnected",
+      payload: client
+    };
+    node.send(msg);
+  });
+
+  server.on('clientDisconnected', function(client) {
+    var msg = {
+      topic: "clientDisconnected",
+      payload: client
+    };
+    node.send(msg);
+  });
+
+  server.on('published', function(packet, client) {
+    var msg = {
+      topic: "published",
+      payload: {
+        packet: packet,
+        client: client
+      }
+    };
+    node.send(msg);
+  });
+
+  server.on('subscribed', function(topic, client) {
+    var msg = {
+      topic: "subscribed",
+      payload: {
+        topic: topic,
+        client: client
+      }
+    };
+    node.send(msg);
+  });
+
+  server.on('unsubscribed', function(topic, client) {
+    var msg = {
+      topic: "unsubscribed",
+      payload: {
+        topic: topic,
+        client: client
+      }
+    };
+    node.send(msg);
+  });
+
+  this.on('close', function() {
+    node.log("Unbinding mosca mqtt server from port: " + this.port);
+    server.close();
+    server.removeAllListeners('clientConnected');
+    server.removeAllListeners('clientDisconnected');
+    server.removeAllListeners('published');
+    server.removeAllListeners('subscribed');
+    server.removeAllListeners('unsubscribed');
+
+  });
+  if (this.dburl && this.dbtype) {
+    var persistence;
+    var onPersistenceReady = function() {
+      node.log('Persistence Ready');
+      persistence.wire(server);
+    }
+    var persistenceOpts = null;
+    switch (this.dbtype.toLowerCase()) {
+      case 'mongo':
+        persistence = mosca.persistence.Mongo({url: this.dburl}, onPersistenceReady);
+        break;
+      case 'levelup':
+        persistence = mosca.persistence.LevelUp({path: this.dburl}, onPersistenceReady);
+        break;
+      default:
+        return node.error('Unexpected dbtype[' + this.dbtype + ']');
+    }
+  }
 }
 RED.nodes.registerType("mosca in", MoscaInNode);
